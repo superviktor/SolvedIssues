d059ab2eb8de6c1ec35382dd183b3bf5152b5b17
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2020-09-01 16:35:14 +0300

    Strongly typed specs and combining

---
+++ SpecificationPattern.Domain/AndSpecification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SpecificationPattern.Domain
+{
+    internal sealed class AndSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> left;
+        private readonly Specification<T> right;
+
+        public AndSpecification(Specification<T> left, Specification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = left.ToExpression();
+            var rightExpression = right.ToExpression();
+
+            var andExpression = Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+
+            return Expression.Lambda<Func<T, bool>>(andExpression, leftExpression.Parameters.Single());
+        }
+    }
+}
---
+++ SpecificationPattern.Domain/NotSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SpecificationPattern.Domain
+{
+    internal sealed class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> operand;
+
+        public NotSpecification(Specification<T> operand)
+        {
+            this.operand = operand;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var expression = operand.ToExpression();
+
+            var notExpression = Expression.Not(expression.Body);
+
+            return Expression.Lambda<Func<T, bool>>(notExpression, expression.Parameters.Single());
+        }
+    }
+}
