6b88d9a8532391c81b18d00ad4eca51d773ad815
superViktor <victor.pryhidko@gmail.com>
2020-11-19 18:02:59 +0200

    Streaming to endpoint

---
+++ StreamFileToApi.Consumer/Controllers/FIleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamFileToApi.Consumer.Controllers
+{
+    [ApiController]
+    public class FIleController : Controller
+    {
+        [HttpPost]
+        [Route("/api/files/receive")]
+        public async Task<IActionResult> Post()
+        {
+            var stream = Request.Body;
+            using StreamReader reader = new StreamReader(stream);
+            string text = await reader.ReadToEndAsync();
+            return Ok(text);
+        }
+    }
+}
