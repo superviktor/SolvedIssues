5b5529870419d1d298c51b718939700877b61639
Viktor Prykhidko <victor.pryhidko@gmail.com>
2021-05-31 22:49:13 +0300

    Pattern matching 2

---
+++ CSharpObserveVersions.Tests/CanalLockTests.cs
@@ -0,0 +1,102 @@
+﻿using CSharpObserveVersions.PatternMatchingCanalLock;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpObserveVersions.Tests
+{
+    [TestFixture]
+    public class CanalLockTests
+    {
+        private CanalLock _canalLock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _canalLock = new CanalLock();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _canalLock = null;
+        }
+
+        [Test]
+        public void OpenHighGate_CanalHasLowWaterLevel_ThrowsException()
+        {
+            _canalLock.SetWaterLevel(WaterLevel.Low);
+
+            Action result = () => _canalLock.SetHighGate(true);
+
+            result.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void OpenHighGate_CanalHasHighWater_Ok()
+        {
+            _canalLock.SetWaterLevel(WaterLevel.High);
+
+            _canalLock.SetHighGate(true);
+
+            _canalLock.HighWaterGateOpen.Should().Be(true);
+        }
+
+        [Test]
+        public void OpenLowGate_CanalHasLowHighLevel_ThrowsException()
+        {
+            _canalLock.SetWaterLevel(WaterLevel.High);
+
+            Action result = () => _canalLock.SetLowGate(true);
+
+            result.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void OpenLowGate_CanalHasLowWater_Ok()
+        {
+            _canalLock.SetWaterLevel(WaterLevel.Low);
+
+            _canalLock.SetLowGate(true);
+
+            _canalLock.LowWaterGateOpen.Should().Be(true);
+        }
+
+        [Test]
+        public void SetHighWater_LowGateIsOpen_ThrowsException()
+        {
+            _canalLock.SetLowGate(true);
+
+            Action result = () => _canalLock.SetWaterLevel(WaterLevel.High);
+
+            result.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void SetLowWater_HighGateIsOpen_ThrowsException()
+        {
+            _canalLock.SetWaterLevel(WaterLevel.High);
+            _canalLock.SetHighGate(true);
+
+            Action result = () => _canalLock.SetWaterLevel(WaterLevel.Low);
+
+            result.Should().Throw<InvalidOperationException>();
+        }
+
+
+        [Test]
+        public void SetLowWater_GatesAreClosed_Ok()
+        {
+            _canalLock.SetLowGate(false);
+            _canalLock.SetHighGate(false);
+
+            _canalLock.SetWaterLevel(WaterLevel.Low);
+
+            _canalLock.WaterLevel.Should().Be(WaterLevel.Low);
+        }
+    }
+}
---
+++ CSharpObserveVersions/PatternMatchingCanalLock/CanalLock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpObserveVersions.PatternMatchingCanalLock
+{
+    public enum WaterLevel
+    {
+        Low,
+        High
+    }
+    public class CanalLock
+    {
+        public WaterLevel WaterLevel { get; private set; } = WaterLevel.Low;
+        public bool LowWaterGateOpen { get; private set; } = false;
+        public bool HighWaterGateOpen { get; private set; } = false;
+
+        public void SetLowGate(bool open)
+        {
+            LowWaterGateOpen = (open, LowWaterGateOpen, WaterLevel) switch
+            {
+                (false, _, _) => false,
+                (true, _, WaterLevel.Low) => true,
+                (true, false, WaterLevel.High) => throw new InvalidOperationException("Cannot open low gate when the water is high"),
+                _ => throw new InvalidOperationException("Invalid internal state"),
+            };
+        }
+
+        public void SetHighGate(bool open)
+        {
+            HighWaterGateOpen = (open, HighWaterGateOpen, WaterLevel) switch
+            {
+                (false, _, _) => false,
+                (true, _, WaterLevel.High) => true,
+                (true, false, WaterLevel.Low) => throw new InvalidOperationException("Cannot open high gate when the water is low"),
+                _ => throw new InvalidOperationException("Invalid internal state"),
+            };
+        }
+
+        public void SetWaterLevel(WaterLevel newLevel)
+        {
+            WaterLevel = (newLevel, WaterLevel, LowWaterGateOpen, HighWaterGateOpen) switch
+            {
+                (WaterLevel.Low, WaterLevel.Low, true, false) => WaterLevel.Low,
+                (WaterLevel.High, WaterLevel.High, false, true) => WaterLevel.High,
+                (WaterLevel.Low, _, false, false) => WaterLevel.Low,
+                (WaterLevel.High, _, false, false) => WaterLevel.High,
+                (WaterLevel.Low, WaterLevel.High, false, true) => throw new InvalidOperationException("Cannot lower water when the high gate is open"),
+                (WaterLevel.High, WaterLevel.Low, true, false) => throw new InvalidOperationException("Cannot raise water when the low gate is open"),
+                _ => throw new InvalidOperationException("Invalid internal state"),
+            };
+        }
+    }
+}
