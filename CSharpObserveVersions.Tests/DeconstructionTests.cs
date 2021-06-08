03e9ce0932939614803efd936b7a8a42ba7ec46e
Viktor Prykhidko <victor.pryhidko@gmail.com>
2021-06-08 21:12:11 +0300

    Property/tuple matching, deconstruction

---
+++ CSharpObserveVersions.Tests/DeconstructionTests.cs
@@ -0,0 +1,21 @@
+﻿using CSharpObserveVersions.Deconstruction;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpObserveVersions.Tests
+{
+    [TestFixture]
+    public class DeconstructionTests
+    {
+        [Test]
+        public void CanDeconstruct()
+        {
+            var person = new Person("viktor", 25);
+
+            (string name, int age) = person;
+
+            name.Should().Be(person.Name);
+            age.Should().Be(person.Age);
+        }
+    }
+}
