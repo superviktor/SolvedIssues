87c4c10de9939f12cc01a5017810bf5577ed3f97
Viktor Prykhidko <victor.pryhidko@gmail.com>
2021-06-09 23:04:41 +0300

    Nullable reference types

---
+++ CSharpObserveVersions.Tests/SurveyRunTests.cs
@@ -0,0 +1,42 @@
+﻿using CSharpObserveVersions.NullableReferenceType;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpObserveVersions.Tests
+{
+    [TestFixture]
+    public class SurveyRunTests
+    {
+        [Test]
+        public void CanAddQuestion()
+        {
+            var surveyRun = new SurveyRun();
+            surveyRun.AddQuestion("Has your code ever thrown a NullReferenceException?", QuestionType.YesNo);
+            surveyRun.AddQuestion(new SurveyQuestion("How many times (to the nearest 100) has that happened?", QuestionType.Number));
+            surveyRun.AddQuestion("What is your favorite color?", QuestionType.Text);
+
+            surveyRun.PerformSurvey(50);
+
+            foreach (var participant in surveyRun.AllParticipants)
+            {
+                if (participant.AnsweredSurvey)
+                {
+                    for (int i = 0; i < surveyRun.Questions.Count; i++)
+                    {
+                        var answer = participant.Answer(i);
+                        Console.WriteLine($"\t{surveyRun.GetQuestion(i).QuestionText} : {answer}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\tNo responses");
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharpObserveVersions/NullableReferenceType/SurveyResponse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable enable
+
+namespace CSharpObserveVersions.NullableReferenceType
+{
+    public class SurveyResponse
+    {
+        private static readonly Random randomGenerator = new Random();
+        private Dictionary<int, string>? _surveyResponses;
+
+        public int Id { get; }
+
+        public SurveyResponse(int id) => Id = id;
+
+        private bool ConsentToSurvey() => randomGenerator.Next(0, 2) == 1;
+
+        public static SurveyResponse GetRandomId() => new SurveyResponse(randomGenerator.Next());
+
+        public bool AnswerSurvey(IEnumerable<SurveyQuestion> questions)
+        {
+            if (ConsentToSurvey())
+            {
+                _surveyResponses = new Dictionary<int, string>();
+                int index = 0;
+                foreach (var question in questions)
+                {
+                    var answer = GenerateAnswer(question);
+                    if (answer != null)
+                    {
+                        _surveyResponses.Add(index, answer);
+                    }
+                    index++;
+                }
+            }
+            return _surveyResponses != null;
+        }
+
+        private string? GenerateAnswer(SurveyQuestion question)
+        {
+            switch (question.Type)
+            {
+                case QuestionType.YesNo:
+                    int n = randomGenerator.Next(-1, 2);
+                    return (n == -1) ? default : (n == 0) ? "No" : "Yes";
+                case QuestionType.Number:
+                    n = randomGenerator.Next(-30, 101);
+                    return (n < 0) ? default : n.ToString();
+                case QuestionType.Text:
+                default:
+                    switch (randomGenerator.Next(0, 5))
+                    {
+                        case 0:
+                            return default;
+                        case 1:
+                            return "Red";
+                        case 2:
+                            return "Green";
+                        case 3:
+                            return "Blue";
+                    }
+                    return "Red. No, Green. Wait.. Blue... AAARGGGGGHHH!";
+            }
+        }
+
+        public bool AnsweredSurvey => _surveyResponses != null;
+
+        public string Answer(int index) => _surveyResponses?.GetValueOrDefault(index) ?? "No answer";
+    }
+}
