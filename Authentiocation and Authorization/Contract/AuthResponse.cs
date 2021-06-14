45bd413fae337c76d84d8c958ac0dd7769f211fc
superViktor <victor.pryhidko@gmail.com>
2021-06-14 17:40:22 +0300

    Custom authorization with jwt token

---
+++ Authentiocation and Authorization/Contract/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Authentiocation_and_Authorization.Contract
+{
+    public class AuthResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Authentiocation and Authorization/JwtMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Authentiocation_and_Authorization
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly AuthSettings _authSettings;
+
+        public JwtMiddleware(RequestDelegate next, IOptions<AuthSettings> authSettings)
+        {
+            _next = next;
+            _authSettings = authSettings.Value;
+        }
+
+        public async Task Invoke(HttpContext context, IUserService userService)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (token != null)
+                AttachUserToContext(context, userService, token);
+
+            await _next(context);
+        }
+
+        private void AttachUserToContext(HttpContext context, IUserService userService, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_authSettings.Secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken) validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                context.Items["User"] = userService.GetById(userId);
+            }
+            catch
+            {
+                // do nothing if jwt validation fails
+                // user is not attached to context so request won't have access to secure routes
+            }
+        }
+    }
+}
