2e51b061f078e024fd48dcca5dc2b8cb7bfb9519
superViktor <victor.pryhidko@gmail.com>
2021-06-23 18:33:26 +0300

    dotnet client for signalr

---
+++ SignalRTest.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRTest.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chatHub")
+                .Build();
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user} says {message}");
+            });
+
+            await connection.StartAsync();
+
+            Console.ReadKey();
+        }
+    }
+}
--- SignalRTest.WebApp/Hubs/ChatHub.cs
+++ SignalRTest.WebApp/Hubs/ChatHub.cs
@@ -1,13 +1,26 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRTest.WebApp.Hubs
 {
+    //[Authorize]
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("SystemMessage", "Connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.Group("groupName").SendAsync("SystemMessage", "Disconnected");
         }
     }
 }
