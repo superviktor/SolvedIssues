7bbbf52573298adc62a9be67e1ded2a612fb764c
superViktor <victor.pryhidko@gmail.com>
2021-07-27 17:03:42 +0300

    Fan-out/fan-in scenario

---
+++ DurableFanOutInt/CopyFileToBlobFunction.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace DurableFanOutInt
+{
+    public class CopyFileToBlobFunction
+    {
+        [FunctionName("CopyFileToBlob")]
+        public static async Task<long> CopyFileToBlob([ActivityTrigger] string filePath, Binder binder, ILogger log)
+        {
+            long byteCount = new FileInfo(filePath).Length;
+
+            // strip the drive letter prefix and convert to forward slashes
+            string blobPath = filePath
+                .Substring(Path.GetPathRoot(filePath).Length)
+                .Replace('\\', '/');
+            string outputLocation = $"backups/{blobPath}";
+
+            log.LogInformation($"Copying '{filePath}' to '{outputLocation}'. Total bytes = {byteCount}.");
+
+            // copy the file contents into a blob
+            using (Stream source = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream destination = await binder.BindAsync<CloudBlobStream>(
+                new BlobAttribute(outputLocation, FileAccess.Write)))
+            {
+                await source.CopyToAsync(destination);
+            }
+
+            return byteCount;
+        }
+    }
+}
---
+++ DurableFanOutInt/GetFileListFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace DurableFanOutInt
+{
+    public class GetFileListFunction
+    {
+        [FunctionName("GetFileList")]
+        public static string[] Run([ActivityTrigger] string rootDirectory, ILogger log)
+        {
+            log.LogInformation($"Searching for files under '{rootDirectory}'...");
+            string[] files = Directory.GetFiles(rootDirectory, "*", SearchOption.AllDirectories);
+            log.LogInformation($"Found {files.Length} file(s) under {rootDirectory}.");
+
+            return files;
+        }
+    }
+}
