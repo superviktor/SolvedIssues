ccb1dd9b66d1f0288620b3c4abe54856325d1ff8
superViktor <victor.pryhidko@gmail.com>
2021-07-28 16:06:54 +0300

    Udf

---
+++ functions.sql
@@ -0,0 +1,24 @@
+﻿--hello world 
+create function dbo.helloworld()
+returns varchar(20)
+as 
+begin
+return 'hello world'
+end
+
+--exec
+select dbo.helloworld()
+
+--params
+create function dbo.f_celsiustofahrenheit(@celcius real)
+returns real
+as 
+begin
+return @celcius*1.8+32
+end
+
+--exec 
+select dbo.f_celsiustofahrenheit(0) as fh
+
+--reusability
+select concat(dbo.helloworld(),' you')
