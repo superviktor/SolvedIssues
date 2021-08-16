56a3259584f644e1b177c520b5b93315e8fd8431
superViktor <victor.pryhidko@gmail.com>
2021-08-16 14:41:37 +0300

    dotnet core distributed cache using redis

---
+++ RedisDistributedCache.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisDistributedCache.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        public WeatherForecastController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public string CachedTimeUtc { get; set; }
+
+        [HttpPost]
+        public async Task Set()
+        {
+            var currentTimeUtc = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+            var encodedCurrentTimeUtc = Encoding.UTF8.GetBytes(currentTimeUtc);
+            var options = new DistributedCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(20));
+            await _distributedCache.SetAsync("key", encodedCurrentTimeUtc, options);
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            CachedTimeUtc = "Cached Time Expired";
+            var encodedCachedTimeUtc = await _distributedCache.GetAsync("key");
+            if (encodedCachedTimeUtc != null)
+                CachedTimeUtc = Encoding.UTF8.GetString(encodedCachedTimeUtc);
+            return CachedTimeUtc;
+        }
+    }
+}
